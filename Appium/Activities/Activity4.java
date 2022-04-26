b814729fdee5e781359f91e45e7949c9d95e6876
Ankhilla <103096451+Ankhilla@users.noreply.github.com>
2022-04-26 23:22:18 +0530

    Update Activity4.java

--- Appium/Activities/Activity4.java
+++ Appium/Activities/Activity4.java
@@ -22,7 +22,7 @@ public class Activity4 {
     public void beforeClass() throws MalformedURLException {
         // Set the Desired Capabilities
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "<device name>");
+        caps.setCapability("deviceName", "9638696078000NT");
         caps.setCapability("platformName", "Android");
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("appPackage", "com.android.contacts");
