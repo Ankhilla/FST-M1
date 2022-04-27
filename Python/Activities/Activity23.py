103d6fb73be6a1bdec394f0faa88b1b13e7f4dd5
Ankhilla <103096451+Ankhilla@users.noreply.github.com>
2022-04-27 22:44:27 +0530

    Update Activity23.py

--- Python/Activities/Activity23.py
+++ Python/Activities/Activity23.py
@@ -3,24 +3,11 @@ import pytest
 
 @pytest.fixture
 def num_list():
-
-
-    list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    
-    # Alternatively
+     list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     # list = list(range(11))
-
     return list
-
-
 def test_sum(num_list):
-
-
     sum = 0
-
-
     for n in num_list:
         sum += n
-
-
     assert sum == 55
